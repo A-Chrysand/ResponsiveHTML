bb123c2042d89198c652cd17bf8e4262069e9ab8
A-Chrysand <56879149+A-Chrysand@users.noreply.github.com>
2021-01-04 23:45:55 +0800

    0104-2345

---
+++ src/PG2-Study/src/se_pg2-3_booklist.js
@@ -0,0 +1,8 @@
+var book = new Object();//创建对象的一个实例
+book.name = "string";
+book.discription = "const string";
+book.tag = ["science","human","nature","tech"];
+
+
+
+//创建方法：是直接全局变量创建，还是函数创建，封装成数组再返回给调用函数????????
